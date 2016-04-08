5b681236a15e0e6a431122e446885ab4767989d1
Milad Bonakdar <miladbonak@gmail.com>
2016-04-08 11:53:46 +0430

    change

--- Cad2D/HscXHelper.cs
+++ Cad2D/HscXHelper.cs
@@ -8,6 +8,68 @@ namespace Cad2D
 {
     public class HscXHelper
     {
+        public Packet<bool>   testModeAXX { set; get; }
+        public Packet<ushort> curPosMMAXX { set; get; }
+        public Packet<ushort> spdReqAXX { set; get; }
+        public Packet<ushort> accAXX { set; get; }
+        public Packet<ushort> decModeAXX { set; get; }
+        public Packet<ushort> manMaxTarPosAXX { set; get; }
+        public Packet<ushort> manMinTarPosAXX { set; get; }
+        public Packet<ushort> bwdKeshAXX { set; get; }
+        public Packet<ushort> fwdKeshAXX { set; get; }
+        public Packet<ushort> bwdMaxDispAXX { set; get; }
+        public Packet<ushort> fwdMaxDispAXX { set; get; }
+        public Packet<bool> jogCalcAutoAXX { set; get; }
+        public Packet<ushort>[] jogArrayAXX { set; get; }
+        public Packet<bool> decAutoAXX { set; get; }
+        public Packet<ushort> decStopAXX { set; get; }
+        public Packet<ushort>[] decArrayAXX { set; get; }
 
+        public HscXHelper()
+        {
+            testModeAXX = new Packet<bool>(3203);
+            curPosMMAXX = new Packet<ushort>(22);
+            spdReqAXX = new Packet<ushort>(254);
+            accAXX = new Packet<ushort>(247);
+            decModeAXX = new Packet<ushort>(248);
+            manMaxTarPosAXX = new Packet<ushort>(243);
+            manMinTarPosAXX = new Packet<ushort>(244);
+            bwdKeshAXX = new Packet<ushort>(28);
+            fwdKeshAXX = new Packet<ushort>(26);
+            bwdMaxDispAXX = new Packet<ushort>(29);
+            fwdMaxDispAXX = new Packet<ushort>(27);
+            jogCalcAutoAXX = new Packet<bool>(3201);
+            decAutoAXX = new Packet<bool>(3204);
+            decStopAXX = new Packet<ushort>(249);
+            jogArrayAXX = new Packet<ushort>[40];
+            decArrayAXX = new Packet<ushort>[40];
+            createPackets(decArrayAXX, 550);
+            createPackets(jogArrayAXX, 350);
+        }
+
+        void createPackets(Packet<ushort>[] array, int address)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Packet<ushort>(address + i);
+            }
+        }
+
+        void parsJogArrayAXX(byte[] continuousData)
+        {
+            parse(continuousData, jogArrayAXX);
+        }
+
+        void parsDecArrayAXX(byte[] continuousData)
+        {
+            parse(continuousData, decArrayAXX);
+        }
+
+        void parse(byte[] continuousData, Packet<ushort>[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i].value = (ushort) (continuousData[i*2 + 1]*256 + continuousData[i*2]);
+
+        }
     }
 }
---
+++ Cad2D/HscYHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cad2D
+{
+    public class HscYHelper
+    {
+        public Packet<bool>      testModeAXY { set; get; }
+        public Packet<ushort>    curPosMMAXY { set; get; }
+        public Packet<ushort>    spdReqAXY { set; get; }
+        public Packet<ushort>    accAXY { set; get; }
+        public Packet<ushort>    decModeAXY { set; get; }
+        public Packet<ushort>    manMaxTarPosAXY { set; get; }
+        public Packet<ushort>    manMinTarPosAXY { set; get; }
+        public Packet<ushort>    bwdKeshAXY     { set; get; }
+        public Packet<ushort>    fwdKeshAXY { set; get; }
+        public Packet<ushort>    bwdMaxDispAXY { set; get; }
+        public Packet<ushort>    fwdMaxDispAXY { set; get; }
+        public Packet<bool>      jogCalcAutoAXY { set; get; }
+        public Packet<ushort>[]  jogArrayAXY { set; get; }
+        public Packet<bool>    decAutoAXY { set; get; }
+        public Packet<ushort>    decStopAXY { set; get; }
+        public Packet<ushort>[]  decArrayAXY { set; get; }
+
+        public HscYHelper()
+        {
+            testModeAXY = new Packet<bool>(3202);
+            curPosMMAXY = new Packet<ushort>(23);
+            spdReqAXY = new Packet<ushort>(255);
+            accAXY = new Packet<ushort>(251);
+            decModeAXY = new Packet<ushort>(252);
+            manMaxTarPosAXY = new Packet<ushort>(245);
+            manMinTarPosAXY = new Packet<ushort>(246);
+            bwdKeshAXY = new Packet<ushort>(32);
+            fwdKeshAXY = new Packet<ushort>(30);
+            bwdMaxDispAXY = new Packet<ushort>(33);
+            fwdMaxDispAXY = new Packet<ushort>(31);
+            jogCalcAutoAXY = new Packet<bool>(3200);
+            decAutoAXY = new Packet<bool>(3206);
+            decStopAXY = new Packet<ushort>(253);
+            jogArrayAXY =  new Packet<ushort>[40];
+            decArrayAXY = new Packet<ushort>[40];
+            createPackets(decArrayAXY, 500);
+            createPackets(jogArrayAXY, 300);
+        }
+
+        void createPackets(Packet<ushort>[] array, int address )
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new Packet<ushort>(address+i);
+            }
+        }
+
+        void parsJogArrayAXY(byte[] continuousData)
+        {
+            parse(continuousData, jogArrayAXY);
+        }
+
+        void parsDecArrayAXY(byte[] continuousData)
+        {
+            parse(continuousData, decArrayAXY);
+        }
+
+        void parse(byte[] continuousData, Packet<ushort>[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                array[i].value = (ushort)(continuousData[i * 2 + 1] * 256 + continuousData[i * 2]);
+        }
+    }
+}
